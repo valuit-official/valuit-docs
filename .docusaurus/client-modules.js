3e7b2950cb2c32c11ec9741439044c57660a8b58
Sourav Santra <55660986+Sourav59580@users.noreply.github.com>
2024-10-29 13:11:31 +0000

    build upload

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/workspaces/valuit-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/workspaces/valuit-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/workspaces/valuit-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/workspaces/valuit-docs/src/css/custom.css"),
-];
