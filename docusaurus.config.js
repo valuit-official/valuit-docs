fb472444fa07562cc80b9c0e20f23f73681ad2ca
product-valuit <product@valuit.com>
2025-06-10 18:05:20 +0000

    update-url-blockridge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   favicon: 'img/favicon-150x150.png',
 
   // Set the production url of your site here
-  url: `https://docs.valuit.com/`,
+  url: `https://docs.blockridge.com/`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: `/`,
